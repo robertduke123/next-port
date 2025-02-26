9ac2eda0f82660f8cea2bef7a34dd7c167b75866
Robert Duke <robduke123@gmail.com>
2025-02-26 22:54:42 +1300

    hero section complete

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Providers } from "./provider";
 
 const geistSans = Geist({
 	variable: "--font-geist-sans",
@@ -26,7 +27,7 @@ export default function RootLayout({
 		<html lang="en">
 			<body
 				className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-				{children}
+				<Providers>{children}</Providers>
 			</body>
 		</html>
 	);
