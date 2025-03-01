30a9bfc5a248c7cc3a28ef9e5eb145677fa715f8
Robert Duke <robduke123@gmail.com>
2025-03-01 16:14:25 +1300

    grid section started

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,18 @@
+import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
+import { FloatingNav } from "@/components/ui/FLoating-nav";
 import Image from "next/image";
+import { FaChrome } from "react-icons/fa6";
 
 export default function Home() {
 	return (
 		<main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10">
 			<div className="max-w-7xl w-full">
+				<FloatingNav
+					navItems={[{ name: "Home", link: "/", icon: <FaChrome /> }]}
+				/>
 				<Hero />
+				<Grid />
 			</div>
 		</main>
 	);
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -18,9 +18,15 @@ const Hero = () => {
 				/>
 				<Spotlight className="top-28 left-80 h-[80vh] w-[50vw]" fill="blue" />
 			</div>
-			<div className="h-screen w-full dark:bg-black-100 bg-white  dark:bg-grid-white/[0.3] bg-grid-black/[0.2] flex items-center justify-center absolute top-0 left-0">
+			<div
+				className="h-screen w-full dark:bg-black-100 bg-white dark:bg-grid-white/[0.03] bg-grid-black-100/[0.2]
+       absolute top-0 left-0 flex items-center justify-center">
 				{/* Radial gradient for the container to give a faded look */}
-				<div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100 bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
+				<div
+					// chnage the bg to bg-black-100, so it matches the bg color and will blend in
+					className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100
+         bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"
+				/>
 			</div>
 
 			<div className="flex justify-center relative my-20 z-10">
@@ -31,7 +37,7 @@ const Hero = () => {
 
 					<TextGenerateEffect
 						className="text-center text-[40px] md:text-5xl lg:text-6xl"
-						words="Transforming Concepts into Seamless Experiences"
+						words="Transforming Concepts into Seamless User Experiences"
 					/>
 
 					<p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
