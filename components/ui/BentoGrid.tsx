69a008d7fecb58d5e344571098b49295dc4061e8
Robert Duke <robduke123@gmail.com>
2025-03-16 18:34:40 +1300

    fixed anchor tags

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -65,7 +65,7 @@ export const BentoGridItem = ({
 	};
 
 	const handleCopy = () => {
-		const text = "hsu@jsmastery.pro";
+		const text = "robduke123@gmail.com";
 		navigator.clipboard.writeText(text);
 		setCopied(true);
 	};
